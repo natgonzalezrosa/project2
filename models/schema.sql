4ed50d748bc64864964db48cd9ca19c104917835
Isabel Parra <isabelparra@Isabels-MacBook-Pro.local>
2018-08-23 21:29:59 -0400

    added schema and seed file

--- models/schema.sql
+++ models/schema.sql
@@ -1,5 +1,25 @@
-DROP DATABASE IF EXISTS exampledb;
-CREATE DATABASE exampledb;
+DROP DATABASE IF EXISTS BikePal_db;
+CREATE DATABASE BikePal_db;
 
-DROP DATABASE IF EXISTS testdb;
-CREATE DATABASE testdb;
+USE BikePal_db;
+
+CREATE TABLE schedule 
+(
+    id int NOT NULL AUTO_INCREMENT,
+    class_time varchar(255) NOT NULL, 
+    class_name varchar(255) NOT NULL, 
+    teacher_name varchar (255) NOT NULL, 
+    capacity int NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE class_roster 
+(
+    id int NOT NULL AUTO_INCREMENT,
+    client_name varchar(255) NOT NULL,
+    email varchar(255) NOT NULL, 
+    firstTime boolean NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (id) REFERENCES schedule(id)
+);
+
---
+++ models/seeds.sql
@@ -0,0 +1,10 @@
+INSERT INTO schedule (class_time, class_name, teacher_name, capacity) VALUES ('8:00', 'Power Cycle', 'Stephanie', 20);
+INSERT INTO schedule (class_time, class_name, teacher_name, capacity) VALUES ('8:00', 'Power Cycle', 'Eric', 15);
+INSERT INTO schedule (class_time, class_name, teacher_name, capacity) VALUES ('9:00', 'Power Cycle', 'Ana', 25);
+INSERT INTO schedule (class_time, class_name, teacher_name, capacity) VALUES ('9:00', 'Power Cycle', 'Rachel', 25);
+INSERT INTO schedule (class_time, class_name, teacher_name, capacity) VALUES ('10:00', 'Power Cycle', 'Eric', 20);
+
+
+INSERT INTO class_roster (client_name, email, firstTime) VALUES ('Alex Schaeffer', 'ascheff@gmail.com', 0);
+INSERT INTO class_roster (client_name, email, firstTime) VALUES ('Mike Bale', 'mbale@gmail.com', 1);
+
