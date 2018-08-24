94a7286e96ec856445149fb0d7608b225621b2a5
Isabel Parra <isabelparra@Isabels-MacBook-Pro.local>
2018-08-24 01:45:10 -0400

    added date column to schema

--- models/schema.sql
+++ models/schema.sql
@@ -6,10 +6,12 @@ USE BikePal_db;
 CREATE TABLE schedule 
 (
     id int NOT NULL AUTO_INCREMENT,
-    class_time varchar(255) NOT NULL, 
-    class_name varchar(255) NOT NULL, 
-    teacher_name varchar (255) NOT NULL, 
-    capacity int NOT NULL,
+    class_date date NOT NULL,
+    start_time varchar(255) NOT NULL, 
+    class_type varchar(255) NOT NULL, 
+    class_level int NOT NULL,
+    instructor varchar (255) NOT NULL, 
+    seats int NOT NULL,
     PRIMARY KEY (id)
 );
 
@@ -18,7 +20,9 @@ CREATE TABLE class_roster
     id int NOT NULL AUTO_INCREMENT,
     client_name varchar(255) NOT NULL,
     email varchar(255) NOT NULL, 
-    firstTime boolean NOT NULL,
+    phone_number varchar(255) NOT NULL,
+    reminder_optIn boolean DEFAULT true,
+    waiver boolean NOT NULL,
     PRIMARY KEY (id),
     FOREIGN KEY (id) REFERENCES schedule(id)
 );
--- models/seeds.sql
+++ models/seeds.sql
@@ -1,10 +1,10 @@
-INSERT INTO schedule (class_time, class_name, teacher_name, capacity) VALUES ('8:00', 'Power Cycle', 'Stephanie', 20);
-INSERT INTO schedule (class_time, class_name, teacher_name, capacity) VALUES ('8:00', 'Power Cycle', 'Eric', 15);
-INSERT INTO schedule (class_time, class_name, teacher_name, capacity) VALUES ('9:00', 'Power Cycle', 'Ana', 25);
-INSERT INTO schedule (class_time, class_name, teacher_name, capacity) VALUES ('9:00', 'Power Cycle', 'Rachel', 25);
-INSERT INTO schedule (class_time, class_name, teacher_name, capacity) VALUES ('10:00', 'Power Cycle', 'Eric', 20);
+INSERT INTO schedule (class_date, start_time, class_type, class_level, instructor, seats) VALUES ('2018-08-01', '8:00', 'Power Cycle', 1, 'Stephanie', 30);
+INSERT INTO schedule (class_date, start_time, class_type, class_level, instructor, seats) VALUES ('2018-08-01', '8:00', 'Power Cycle', 2, 'Eric', 35);
+INSERT INTO schedule (class_date, start_time, class_type, class_level, instructor, seats) VALUES ('2018-08-01', '9:00', 'Power Cycle', 1, 'Ana', 30);
+INSERT INTO schedule (class_date, start_time, class_type, class_level, instructor, seats) VALUES ('2018-08-01', '9:00', 'Power Cycle', 2, 'Rachel', 35);
+INSERT INTO schedule (class_date, start_time, class_type, class_level, instructor, seats) VALUES ('2018-08-01', '10:00', 'Power Cycle', 1, 'Eric', 35);
 
 
-INSERT INTO class_roster (client_name, email, firstTime) VALUES ('Alex Schaeffer', 'ascheff@gmail.com', 0);
-INSERT INTO class_roster (client_name, email, firstTime) VALUES ('Mike Bale', 'mbale@gmail.com', 1);
+INSERT INTO class_roster (client_name, email, phone_number, reminder_optIn, waiver) VALUES ('Alex Schaeffer', 'ascheff@gmail.com', '335642324', true, true);
+INSERT INTO class_roster (client_name, email, phone_number, reminder_optIn, waiver) VALUES ('Mike Bale', 'mbale@gmail.com', '7864563424', true, true);
 
