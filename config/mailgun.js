6ed844420675ff9eb1f5c66aaff40a85e9ae9d2b
natgonzalezrosa <natgonzalezrosa@gmail.com>
2018-09-03 19:44:18 -0400

    added Frank's mailgun, added handlebars and fixed bug

---
+++ config/mailgun.js
@@ -0,0 +1,6 @@
+module.exports = {
+    "key": "677204173d3c93a0f6b32cfcb83454ac-a4502f89-7b6ce0ab",
+    "domain": "m.bike-pal.com",
+    "contact": "franksantiesteban@yahoo.com",
+    "from": "info@bike-pal.com"
+}
