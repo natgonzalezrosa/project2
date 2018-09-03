d07774695ecdc56cd2a44c41ee067f4fc8d82430
Frank Santiesteban <franksantiesteban@Franks-MacBook-Pro.local>
2018-09-03 13:29:31 -0400

    mailgun.js mailRoutes.js

---
+++ routes/mailRoutes.js
@@ -0,0 +1,28 @@
+var mailgun = require('../config/mailgun');
+var key = mailgun.key;
+var domain = mailgun.domain;
+var contact = mailgun.contact;
+
+console.log(mailgun, key, domain);
+
+module.exports = function(app) {
+
+  app.post("/api/contact", function(req, res) {
+    var mailgun = require('mailgun-js')({
+      apiKey: key,
+      domain: domain
+    });
+
+    var data = {
+      from: 'Test User <info@bike-pal.com>',
+      to: contact,
+      subject: req.body.subject,
+      text: req.body.message + " Contact email: " + req.body.contact
+    };
+
+    mailgun.messages().send(data, function(error, body) {
+      console.log(body);
+    });
+  });
+
+}
